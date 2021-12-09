e290f22ea16a236f910393828fa7ef86a108a904
dantelentsoe <dllentsoe@gmail.com>
2021-12-09 22:03:57 +0200

    feat: mongoDB connection

--- index.ts
+++ index.ts
@@ -1,13 +1,22 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 // routes
 const protectedRoute = require("./routes/auth");
 
+dotenv.config();
+
+// Connect to DB and
+mongoose.connect(process.env.DB_Connection, console.log("Connected to DB"));
+
+// middleware
+app.use(express.json());
+
 // route middleware
 
-app.use("api/user", protectedRoute);
+app.use("/api/user", protectedRoute);
 
 app.listen(3000, () => {
   console.log("Server Running");
