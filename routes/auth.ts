9650f0c3f9e56534289c021927f799819eb4ee08
dantelentsoe <dllentsoe@gmail.com>
2021-12-12 13:39:05 +0200

    refactor: validation addition

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,7 +1,30 @@
 const router = require("express").Router();
 const User = require("../model/user/user");
 
+// Validation
+const Joi = require("joi");
+
+/*
+*
+*
+*
+  //TODO : Fix validation
+const schemaValidation = Joi.object({
+  name: Joi.string().min(6).required(),
+  email: Joi.string().min(8).required().email(),
+  password: Joi.string().min(6).required(),
+});
+*
+*
+*/
 router.post("/register", async (request: any, response: any) => {
+  console.log("types check ", response);
+  //TODO : Fix validation
+  // const validaitonCheck = Joi.validaiton(request.body);
+
+  //TODO : Fix validation
+  //response.send(validaitonCheck);
+
   const user = new User({
     name: request.body.name,
     email: request.body.email,
