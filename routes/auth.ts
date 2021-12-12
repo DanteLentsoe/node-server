3457ef1c9974859954615cd36a59ebc8ae55116d
dantelentsoe <dllentsoe@gmail.com>
2021-12-12 15:34:19 +0200

    feat: login feature

--- routes/auth.ts
+++ routes/auth.ts
@@ -4,6 +4,7 @@ const bcrypt = require("bcrypt");
 //TODO : Fix validation
 // const { loginValidation, registerValidation } = require("../utils/validation");
 
+// Register User
 router.post("/register", async (request: any, response: any) => {
   //TODO : Fix validation
   // const {error} = registerValidation(request.body)
@@ -30,10 +31,34 @@ router.post("/register", async (request: any, response: any) => {
 
   try {
     const savedUser = await user.save();
-    response.send(savedUser);
+    response.send({ user: user._id });
   } catch (err) {
     response.status(400).send(err);
   }
+});
+
+// Login User
+router.post("/login", async (request: any, response: any) => {
+  // check user email in DB
+  const userExist = await User.findOne({
+    email: request.body.email,
+  });
+
+  if (!userExist) {
+    return response.status(400).send("User email does not exist");
+  }
+
+  //check password correct
+  const validPassword = await bcrypt.compare(
+    request.body.password,
+    userExist.password
+  );
+
+  if (!validPassword) {
+    return response.status(400).send("User password is not valid");
+  }
+
+  response.send("Logged In");
 });
 
 module.exports = router;
